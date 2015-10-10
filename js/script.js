be812f363b42e336a1fa02977125fc3c71aa8612
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-10-09 22:55:06 -0700

    Added functions for buttons and drag/drop features

--- js/script.js
+++ js/script.js
@@ -1,13 +1,60 @@
 // Load script on ready
-$('document').ready(function() {
+//$('document').ready(function() {
 
-// Make an array containing different arrays of rubbish items by category
+// First, set global variables
+
+// Make a var to track items generated
+
+var itemsGenerated = 0;
+
+// Make a var to track items successfully sorted
+
+var itemsSorted = 0;
+
+// Make a var to keep track of current round
+
+var currentRound = 0;
+
+// Make intervalID for current round
+
+var intervalID = -1;
+
+// Next, set properties for DOM elements
+
+// Make bins droppable (use hoverClass option for tutorial mode)
+// Additionally, make items hide when dropped and increment items sorted
+
+$( "#blue-bin" ).droppable({
+	accept: ".blue-item",
+	drop: function(event, ui) {
+		ui.draggable.remove();
+		itemsSorted++;
+	}
+}); 
+
+$( "#green-bin" ).droppable({
+	accept: ".green-item",
+	drop: function(event, ui) {
+		ui.draggable.remove();
+		itemsSorted++;
+	}
+});
+
+$( "#gray-bin" ).droppable({
+	accept: ".gray-item",
+	drop: function(event, ui) {
+		ui.draggable.remove();
+		itemsSorted++;
+	}
+});
+
+// Now, make an array containing different arrays of rubbish items by category
 
 var rubbish = {
 	"compostable" : ["banana peel", "eggshell", "tea bag", "flowers", "paper plate"],
 	"recycling" : ["milk carton", "cereal box", "paper cup", "yogurt container", "aluminum can"],
 	"garbage": ["aluminum foil", "plastic wrap", "foam carton", "rubber band", "plastic fork"],
-}; // tip: to access banana peel, you need rubbish["compostable"][0]
+}; // key: to access banana peel, you need rubbish["compostable"][0]
 
 // Make a function to create a random blue item with blue class
 // if tutorial mode == true, set aura
@@ -27,56 +74,116 @@ var grayItem = function() {
 $('#trash-chute').append('<div class="gray-item">' + rubbish["garbage"][Math.floor((Math.random()*rubbish["garbage"].length))] + '</div>');
 };
 
-// Make a var to track items generated
-var itemsGenerated = 0;
+// Make function to make newly generated items draggable
 
-// Make a var to track items successfully sorted
+var makeDraggable = function() {
 
-var itemsSorted = 0;
+	$( ".blue-item" ).draggable({
+		containment: $('#game'),
+		cursor: 'move',
+		revert: 'invalid'
+	});
 
-// Make a var to keep track of current round
+	$( ".green-item" ).draggable({
+		containment: $('#game'),
+		cursor: 'move',
+		revert: 'invalid'
+	});
 
-var currentRound = 0;
+	$( ".gray-item" ).draggable({
+		containment: $('#game'),
+		cursor: 'move',
+		revert: 'invalid'
+	});
 
-// Make a function to randomly generate items at a set interval with a 1 minute timeout
-// At timeout, check for winner
+};
+
+// Make a function that randomly generates a number from 0-3 to determine the array from which to draw
 
 var getRandomCategory = function() {
 	return Math.floor((Math.random()*3));
 };
 
-var generateItem = function() {
-	var randomCategory = getRandomCategory();
+// Make a function to randomly generate items
 
-	if (randomCategory == 0) {
-		console.log('Blue');
-		return blueItem();
-	} else if (randomCategory == 1) {
-		console.log('Green');
-		return greenItem();
-	} else if (randomCategory == 2) {
-		console.log('Gray');
-		return grayItem();
-	};
+var generateItem = function() {
+
+		var randomCategory = getRandomCategory();
+
+		if (randomCategory == 0) {
+			console.log('Blue');
+			return blueItem();
+		} else if (randomCategory == 1) {
+			console.log('Green');
+			return greenItem();
+		} else if (randomCategory == 2) {
+			console.log('Gray');
+			return grayItem();
+		};
+
+		itemsGenerated++;
+
+}; // end generateItem function
+
+
+
+// Make function to play game (set interval for generateItem) at a set interval with a 1 minute timeout
+// At timeout, check for winner
+// Assign to play button
+
+var playGame = function() {
+
+	intervalID = setInterval(function() {
+		generateItem();
+		makeDraggable();
+		// check winner
+	}, 2000);
 };
 
-// Make items droppable and trash cans receivable
+$('#play-button').on("click", function(event) {
+	event.preventDefault();
+	playGame();
+});
 
-// Make items hide when dropped and increment items sorted
+// Make function to stop game and assign to stop button
 
-// Make function to play game
+var stopGame = function() {
+	clearInterval(intervalID);
+	$('#trash-chute').html('');
+};
 
-// Make function to restart game
+$('#stop-button').on("click", function(event) {
+	event.preventDefault();
+	stopGame();
+});
 
-// Make function to stop game
+// Make function to restart game and assign to reset button
+
+var restartGame = function() {
+	event.preventDefault();
+	stopGame();
+	playGame();
+};
+
+$('#restart-button').on("click", function(event) {
+	event.preventDefault();
+	restartGame();
+});
+
 
 // Make function to enter tutorial mode
 
-// Make play button, tutorial mode button, reset button, and stop button
-
 // Function to check for winner 
+
+// var checkWinner = function() {
+// 	if (itemsSorted == itemsGenerated) {
+// 		alert('You win!');
+// 		stopGame();
+// 	}
+// }
 // If win, popup win and show button to go to next round or stop, if Lose, popup lose and give option to replay or stop
 
 // Function next round, increment item generation interval, increment round counter
+// Or you can do an easy, medium, hard mode!
 
-}); // end document load function
+//}); // end document load function
